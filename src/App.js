5345868cfd54a5dc4422269005515841c114531e
unknown <cookieKang@gmail.com>
2019-08-04 11:13:31 +0900

    Sample Component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = "Hello React App"
+  const num = 10
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {name} {num + 100}
         </p>
         <a
           className="App-link"
---
+++ src/Sample.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Sample extends Component {
+
+    render() {
+        return (
+            <div>
+                <h2>This is sample component</h2>
+            </div>
+        )
+    }
+}
